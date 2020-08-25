276c6573c587db3ea02718e6f65a5e110ee6f086
Tan Theng Lee <thengleetan@gmail.com>
2020-08-25 14:52:59 +0800

    Add router with sub routes

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,20 @@ import { Router } from 'express'
 
 const router = Router()
 
+// mock controller
+const controller = (req, res) => {
+  res.send({ message: 'hello' })
+}
+
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
+
 export default router
