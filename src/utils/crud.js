a33c9f694f95dfbce3f9c49dfcedf4586ec5063b
Tan Theng Lee <thengleetan@gmail.com>
2020-08-28 22:39:12 +0800

    Implement crud util

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,51 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  try {
+    const data = await model.findById(req.params.id)
+    res.status(200).json({ data })
+  } catch (err) {
+    res.status(400)
+  }
+}
 
-export const getMany = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  try {
+    const data = await model.find({ createdBy: req.user._id })
+    res.status(200).json({ data })
+  } catch (err) {
+    res.status(400)
+  }
+}
 
-export const createOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  try {
+    const data = await model.create({ createdBy: req.user._id, ...req.body })
+    res.status(201).json({ data })
+  } catch (err) {
+    res.status(400)
+  }
+}
 
-export const updateOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  try {
+    const data = await model.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    )
+    res.status(200).json({ data })
+  } catch (err) {
+    res.status(400)
+  }
+}
 
-export const removeOne = model => async (req, res) => {}
+export const removeOne = model => async (req, res) => {
+  try {
+    const data = await model.findByIdAndDelete(req.params.id)
+    res.status(200).json({ data })
+  } catch (err) {
+    res.status(400)
+  }
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
